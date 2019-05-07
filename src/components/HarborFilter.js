ef46fd0c0206804806f7319e9c9fe7c639b9149f
martinlundin <mlundin1994@gmail.com>
2019-05-07 12:56:53 +0200

    Add default harbor to be the first one. (resolve #11) Add active class to harbor that is active (resolve #10)

--- src/components/HarborFilter.js
+++ src/components/HarborFilter.js
@@ -1,11 +1,26 @@
 import React from 'react';
 
 class HarborFilter extends React.Component {
+    constructor(props) {
+        super(props);
+
+        let defaultHarbor = this.props.Harbors[0].Name;
+        this.state = {
+            "active": defaultHarbor
+        };
+        this.props.changeHarbor(defaultHarbor);
+    }
+
     render() {
         return (
             <div className={`HarborFilter`}>
                 {this.props.Harbors.map((Harbor) => (
-                    <span key={Harbor.Id} onClick={this.props.changeHarbor.bind(this, Harbor.Name)}>
+
+                    <span key={Harbor.Id} className={this.state.active === Harbor.Name ? "active" : ""}
+                          onClick={() => {
+                              this.props.changeHarbor(Harbor.Name);
+                              this.setState({"active": Harbor.Name})
+                          }}>
                         {Harbor.Name}
                     </span>
                 ))}
