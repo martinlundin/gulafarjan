aa749a773e8a3fdb44c3c3f24864acf76ccab9ab
martinlundin <mlundin1994@gmail.com>
2019-09-14 23:40:06 +0200

    Fix spelling

--- src/components/Home.js
+++ src/components/Home.js
@@ -25,7 +25,7 @@ class Home extends React.Component {
                     <p>Denna webbapp är utvecklad för att göra det lite enklare att veta om hur lång tid färjan går. Alla Trafikverkets bilfärjor i landet finns med här och genom att söka eller välja vilken rutt på kartan kommer man snabbt se när de nästkommande färjorna ska gå.</p>
                     <p>Några av de mer populära färjelederna på Trafikverkets Färjeräderi är: Hönöleden, Gullmarsleden, Vaxholmsleden, Ljusteröleden, Ekeröleden, Svanesundsleden, Korhallsleden, Björköleden, Furusundsleden, Fårösundsleden, Gräsöleden, Blidöleden, Adelsöleden. Det är bara att nämna några, sök i rutan för att hitta din färjeled.</p>
                     <h2>Kontakt</h2>
-                    <p>Appen är utvecklat ideelt. Har du några problem, förslag på förbättringar eller vill du hjälpa till att programmera så besök gärna open-source projektet <a href={"https://github.com/martinlundin/gulafarjan"}>github.com/martinlundin/gulafarjan</a></p>
+                    <p>Appen är utvecklad ideelt. Har du några problem, förslag på förbättringar eller vill du hjälpa till att programmera så besök gärna open-source projektet <a href={"https://github.com/martinlundin/gulafarjan"}>github.com/martinlundin/gulafarjan</a></p>
                     <p>Om du eller dit bolag behöver utveckling av hemsidor eller appar kan ni höra av er till <a href="mailto:martin@fokusiv.com">martin@fokusiv.com</a></p>
                 </div>
             </div>
